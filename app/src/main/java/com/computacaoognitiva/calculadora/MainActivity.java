38d42e821b1b93a345c2e161bf7b0057c20fa09e
Alex Fraga Belo <alexbelo2.afb@gmail.com>
2018-04-02 09:37:34 -0300

    Primeiro Commit

---
+++ app/src/main/java/com/computacaoognitiva/calculadora/MainActivity.java
@@ -0,0 +1,56 @@
+package com.computacaoognitiva.calculadora;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void clickMC(View view) {
+    }
+
+    public void clickMPlus(View view) {
+    }
+
+    public void clickMLess(View view) {
+    }
+
+    public void clickMR(View view) {
+    }
+
+    public void clickAC(View view) {
+    }
+
+    public void clickPlusLess(View view) {
+    }
+
+    public void clickPercent(View view) {
+    }
+
+    public void clickDivision(View view) {
+    }
+
+    public void clickNumber(View view) {
+    }
+
+    public void clickMutiplication(View view) {
+    }
+
+    public void clickLess(View view) {
+    }
+
+    public void clickPlus(View view) {
+    }
+
+    public void clickSplit(View view) {
+    }
+
+    public void clickEqual(View view) {
+    }
+}
