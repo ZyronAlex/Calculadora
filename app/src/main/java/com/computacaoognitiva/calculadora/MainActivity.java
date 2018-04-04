2e81505fdfdf5e77de465fe5fb81248e3d8087a4
Alex Fraga Belo <alexbelo2.afb@gmail.com>
2018-04-04 14:10:58 -0300

    modulos de calculo

--- app/src/main/java/com/computacaoognitiva/calculadora/MainActivity.java
+++ app/src/main/java/com/computacaoognitiva/calculadora/MainActivity.java
@@ -1,7 +1,7 @@
 package com.computacaoognitiva.calculadora;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
@@ -9,25 +9,31 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.computacaoognitiva.calculadora.model.AplCalcular;
-import com.computacaoognitiva.calculadora.view.ViewConsole;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 import static com.computacaoognitiva.calculadora.model.AplTradutorNotacao.ocidentalToPolonesa;
+import static com.computacaoognitiva.calculadora.model.AplTradutorNotacao.processaNotacao;
 
 public class MainActivity extends AppCompatActivity {
     private double M, value;
     private boolean error;
-    private TextView txResult;
-    private String notacao = "";
+    private TextView txResult,textM;
+    private String notacao;
     private AplCalcular aplCalcular = new AplCalcular();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        notacao = "";
+        value = 0;
+        error = false;
+
+        textM = findViewById(R.id.m);
+        textM.setVisibility(View.GONE);
 
         txResult = findViewById(R.id.result);
         txResult.addTextChangedListener(new TextWatcher() {
@@ -43,28 +49,34 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void afterTextChanged(Editable s) {
-                if (s.length() > 20)
-                    txResult.setText(R.string.erro);
-                resultToValue();
+                if (s.length() > 20) {
+                    error = true;
+                    valueToResult();
+                }
             }
         });
     }
 
     public void clickMC(View view) {
         M = 0;
+        textM.setVisibility(View.GONE);
     }
 
     public void clickMPlus(View view) {
-        if(!error){
-        resultToValue();
-        M += value;
+        if (!error) {
+            resultToValue();
+            M += value;
+            if(M != 0)
+            textM.setVisibility(View.VISIBLE);
         }
     }
 
     public void clickMLess(View view) {
-        if(!error) {
+        if (!error) {
             resultToValue();
             M -= value;
+            if(M != 0)
+                textM.setVisibility(View.VISIBLE);
         }
     }
 
@@ -75,14 +87,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void clickAC(View view) {
-        M = 0;
         value = 0;
-        valueToResult();
         error = false;
+        valueToResult();
     }
 
     public void clickPlusLess(View view) {
-        if(!error) {
+        if (!error) {
             resultToValue();
             value *= (-1);
             valueToResult();
@@ -90,42 +101,53 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void clickPercent(View view) {
-        
+        if (!error)
+            txResult.setText(String.format("%s%%", notacao));
     }
 
     public void clickDivision(View view) {
-        txResult.setText(String.format("%s/", notacao));
+        if (!error)
+            txResult.setText(String.format("%s/", notacao));
     }
 
     public void clickNumber(View view) {
-        if(notacao )
-        txResult.setText(String.format("%s%s", notacao, ((Button) view).getText().toString()));
+        if (!error) {
+            if (notacao.equals("0") || notacao.equals("0.0") || notacao.equals(R.string.erro))
+                notacao = "";
+            txResult.setText(String.format("%s%s", notacao, ((Button) view).getText().toString()));
+        }
     }
 
     public void clickMultiplication(View view) {
-        txResult.setText(String.format("%s*", notacao));
+        if (!error)
+            txResult.setText(String.format("%s*", notacao));
     }
 
     public void clickLess(View view) {
-        txResult.setText(String.format("%s-", notacao));
+        if (!error)
+            txResult.setText(String.format("%s-", notacao));
     }
 
     public void clickPlus(View view) {
-        txResult.setText(String.format("%s+", notacao));
+        if (!error)
+            txResult.setText(String.format("%s+", notacao));
     }
 
     public void clickSplit(View view) {
-        txResult.setText(String.format("%s,", notacao));
+        if (!error)
+            txResult.setText(String.format("%s,", notacao));
     }
 
     public void clickEqual(View view) {
-        valueToResult();
+        if (!error) {
+            resultToValue();
+            valueToResult();
+        }
     }
 
     private void valueToResult() {
-        resultToValue();
-        if(!error)
-        txResult.setText(String.valueOf(value));
+        if (!error)
+            txResult.setText(String.valueOf(value));
         else
             txResult.setText(R.string.erro);
     }
@@ -134,71 +156,77 @@ public class MainActivity extends AppCompatActivity {
         if (notacao.equals(R.string.erro)) {
             value = 0;
         } else {
-            numbers = new ArrayList<>();
-            operators = new ArrayList<>();
-            StringBuilder number = new StringBuilder();
-            for (Character s : notacao.toCharArray()) {
-                if (simbols.contains(s)) {
-                    numbers.add(Double.parseDouble(number.toString()));
-                    number = new StringBuilder();
-                    operators.add(s);
-                } else {
-                    number.append(s);
-                }
-            }
+            try {
+                notacao = ocidentalToPolonesa(notacao);
+                value = aplCalcular.calcular(notacao);
 
-            for (int i = 0; i < operators.size(); i++) {
-                if (operators.equals('*')) {
-                    double p1 = numbers.get(i);
-                    double p2 = numbers.get(i++);
-                    double result = p1 * p2;
-                    numbers.remove(i++);
-                    numbers.remove(i);
-                    operators.remove(i);
-                    numbers.add(i < 0 ? 0 : i, result);
-                }
+//                ArrayList<Double> numbers = new ArrayList<>();
+//                ArrayList<String> operators = new ArrayList<>();
+//                ArrayList<String> simbols = new ArrayList<String>(Arrays.asList("*", "/", "+", "-", "%"));
+//                StringBuilder number = new StringBuilder();
+//                for (Character s : notacao.toCharArray()) {
+//
+//                    if (simbols.contains(s.toString())) {
+//                        numbers.add(Double.parseDouble(number.toString()));
+//                        number = new StringBuilder();
+//                        operators.add(s.toString());
+//                    } else {
+//                        number.append(s);
+//                    }
+//                }
+//                if(!number.equals(""))
+//                numbers.add(Double.parseDouble(number.toString()));
+//
+//
+//                for (int i = 0; i < operators.size(); i++) {
+//                    if (operators.get(i).equals("*")) {
+//                        double p1 = numbers.get(i);
+//                        double p2 = numbers.get(i++);
+//                        double result = p1 * p2;
+//                        numbers.remove(i++);
+//                        numbers.remove(i);
+//                        operators.remove(i);
+//                        numbers.add(i < 0 ? 0 : i, result);
+//                    }
+//                }
+//                for (int i = 0; i < operators.size(); i++) {
+//                    if (operators.get(i).equals("/")) {
+//                        double p1 = numbers.get(i);
+//                        double p2 = numbers.get(i++);
+//                        double result = p1 / p2;
+//                        numbers.remove(i++);
+//                        numbers.remove(i);
+//                        operators.remove(i);
+//                        numbers.add(i < 0 ? 0 : i, result);
+//                    }
+//                }
+//                for (int i = 0; i < operators.size(); i++) {
+//                    if (operators.get(i).equals("+")) {
+//                        double p1 = numbers.get(i);
+//                        double p2 = numbers.get(i++);
+//                        double result = p1 + p2;
+//                        numbers.remove(i+1);
+//                        numbers.remove(i);
+//                        operators.remove(i);
+//                        numbers.add(i < 0 ? 0 : i, result);
+//                    }
+//                }
+//                for (int i = 0; i < operators.size(); i++) {
+//                    if (operators.get(i).equals("-")) {
+//                        double p1 = numbers.get(i);
+//                        double p2 = numbers.get(i++);
+//                        double result = p1 - p2;
+//                        numbers.remove(i++);
+//                        numbers.remove(i);
+//                        operators.remove(i);
+//                        numbers.add(i < 0 ? 0 : i, result);
+//                    }
+//                }
+            } catch (Exception e) {
+                error = true;
+                valueToResult();
+                e.printStackTrace();
             }
-            for (int i = 0; i < operators.size(); i++) {
-                if (operators.equals('/')) {
-                    double p1 = numbers.get(i);
-                    double p2 = numbers.get(i++);
-                    double result = p1 / p2;
-                    numbers.remove(i++);
-                    numbers.remove(i);
-                    operators.remove(i);
-                    numbers.add(i < 0 ? 0 : i, result);
-                }
-            }
-            for (int i = 0; i < operators.size(); i++) {
-                if (operators.equals('+')) {
-                    double p1 = numbers.get(i);
-                    double p2 = numbers.get(i++);
-                    double result = p1 + p2;
-                    numbers.remove(i++);
-                    numbers.remove(i);
-                    operators.remove(i);
-                    numbers.add(i < 0 ? 0 : i, result);
-                }
-            }
-            for (int i = 0; i < operators.size(); i++) {
-                if (operators.equals('-')) {
-                    double p1 = numbers.get(i);
-                    double p2 = numbers.get(i++);
-                    double result = p1 - p2;
-                    numbers.remove(i++);
-                    numbers.remove(i);
-                    operators.remove(i);
-                    numbers.add(i < 0 ? 0 : i, result);
-                }
-            }
-        }
-        try{
-            notacao = ocidentalToPolonesa(notacao);
-            value = aplCalcular.calcular(notacao);
-        }
-        catch(Exception e){
-            view.printError();
-            e.printStackTrace();
         }
     }
 
